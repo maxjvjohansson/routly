1a9fc072713884aeefe981b426bfe330378101eb
maxjvjohansson <max.jv.johansson@gmail.com>
2025-10-14 18:37:34 +0200

    Add: InputField component for RN/Expo

--- apps/mobile/src/app/index.tsx
+++ apps/mobile/src/app/index.tsx
@@ -1,5 +1,7 @@
 import { Button } from "src/components/Button/Button";
 import styled from "styled-components/native";
+import { InputField } from "src/components/InputField/InputField";
+import { useState } from "react";
 
 const Container = styled.View`
   flex: 1;
@@ -13,9 +15,27 @@ const Title = styled.Text`
 `;
 
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <Container>
       <Title>Routly App</Title>
+      <InputField
+        label="Email"
+        placeholder="Enter your email"
+        type="email"
+        value={email}
+        onChange={setEmail}
+        fullWidth
+      />
+      <InputField
+        label="Password"
+        placeholder="Enter your password"
+        type="password"
+        value={password}
+        onChange={setPassword}
+        fullWidth
+      />
       <Button label="Login" variant="solid" color="teal" />
       <Button label="Login" variant="outline" color="orange" />
       <Button label="Login" variant="outline" color="orange" disabled />
