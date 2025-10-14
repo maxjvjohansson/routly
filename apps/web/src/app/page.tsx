9622d487d0598f93cc3d20ae14ca91ae905f0e44
maxjvjohansson <max.jv.johansson@gmail.com>
2025-10-14 20:44:49 +0200

    Add: dynamic AuthForm component to NextJs app

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,36 +1,25 @@
 "use client";
-import { InputField } from "src/components/InputField/InputField";
-import { Button } from "../components/Button/Button";
+
 import styled from "styled-components";
-import { useState } from "react";
+import AuthForm from "src/components/AuthForm/AuthForm";
 
 const Title = styled.h1`
   color: #0598a8;
 `;
 
-export default function Home() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+export default function HomePage() {
+  const handleSubmit = async (
+    email: string,
+    password: string,
+    fullName?: string
+  ) => {
+    console.log("Submitted:", { email, password, fullName });
+    alert(`Success! Mode: signup, Email: ${email}`);
+  };
   return (
     <div>
       <Title>Routly Web</Title>
-      <InputField
-        label="Email"
-        placeholder="Enter your email"
-        type="email"
-        value={email}
-        onChange={setEmail}
-      />
-      <InputField
-        label="Password"
-        placeholder="Enter your password"
-        type="password"
-        value={password}
-        onChange={setPassword}
-      />
-      <Button label="Login" variant="solid" color="teal" />
-      <Button label="Login" variant="outline" color="orange" />
-      <Button label="Login" variant="outline" color="orange" disabled />
+      <AuthForm mode="signup" onSubmit={handleSubmit} />
     </div>
   );
 }
