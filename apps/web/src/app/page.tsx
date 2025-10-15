ff0c37d6cd6fec5d5f01b5505c51c3041fb1f2f2
maxjvjohansson <max.jv.johansson@gmail.com>
2025-10-15 10:46:45 +0200

    Update: AuthForm, custom auth hook and signup/login pages in Next app

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,25 +1,15 @@
 "use client";
 
 import styled from "styled-components";
-import AuthForm from "src/components/AuthForm/AuthForm";
 
 const Title = styled.h1`
   color: #0598a8;
 `;
 
 export default function HomePage() {
-  const handleSubmit = async (
-    email: string,
-    password: string,
-    fullName?: string
-  ) => {
-    console.log("Submitted:", { email, password, fullName });
-    alert(`Success! Mode: signup, Email: ${email}`);
-  };
   return (
     <div>
       <Title>Routly Web</Title>
-      <AuthForm mode="signup" onSubmit={handleSubmit} />
     </div>
   );
 }
