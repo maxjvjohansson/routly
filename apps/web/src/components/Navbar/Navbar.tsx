29581947249f08f84774f8d4b6176ab0daee1704
maxjvjohansson <max.jv.johansson@gmail.com>
2025-10-21 14:26:52 +0200

    Add: Box-shadow under Navbar for better separation when user scrolls down

--- apps/web/src/components/Navbar/Navbar.tsx
+++ apps/web/src/components/Navbar/Navbar.tsx
@@ -1,12 +1,20 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import { NavbarContainer, Logo, LogoLink } from "./styles";
 import DesktopNav from "./DesktopNav";
 import MobileMenu from "./MobileMenu";
 
 export default function Navbar() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => setScrolled(window.scrollY > 10);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
-    <NavbarContainer>
+    <NavbarContainer $scrolled={scrolled}>
       <LogoLink href="/" passHref>
         <Logo>Routly</Logo>
       </LogoLink>
