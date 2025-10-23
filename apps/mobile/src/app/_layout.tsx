0011ad1df034008175fc49fd92e7ca828e59e62d
maxjvjohansson <max.jv.johansson@gmail.com>
2025-10-23 12:42:24 +0200

    Update: Use of AuthContext in Expo app, also rendering of user initial on avatar in Expo

--- apps/mobile/src/app/_layout.tsx
+++ apps/mobile/src/app/_layout.tsx
@@ -1,15 +1,18 @@
 import { Stack } from "expo-router";
 import { ThemeProvider } from "styled-components/native";
 import { nativeTheme } from "@routly/ui/theme/native";
+import { AuthProvider } from "@routly/lib/context/AuthContext";
 
 export default function RootLayout() {
   return (
     <ThemeProvider theme={nativeTheme as any}>
-      <Stack
-        screenOptions={{
-          headerShown: false,
-        }}
-      />
+      <AuthProvider>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+          }}
+        />
+      </AuthProvider>
     </ThemeProvider>
   );
 }
