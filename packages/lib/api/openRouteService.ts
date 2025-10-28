97a0834f5fb6071f7ea7a940bfe72012e6ee5680
maxjvjohansson <max.jv.johansson@gmail.com>
2025-10-28 10:29:20 +0100

    Update: ORS API fetch function to calculate roundtrip distance in response if no endpoint is given (and no Distance from Selector is set)

--- packages/lib/api/openRouteService.ts
+++ packages/lib/api/openRouteService.ts
@@ -1,5 +1,5 @@
 const ORS_BASE_URL = "https://api.openrouteservice.org/v2";
-const ORS_API_KEY = process.env.ORS_API_KEY || process.env.ORS_API_KEY;
+const ORS_API_KEY = process.env.ORS_API_KEY;
 
 if (!ORS_API_KEY) {
   console.warn("Missing ORS API key in environment variables");
@@ -12,23 +12,40 @@ export type ORSProfile =
   | "foot-hiking";
 
 export async function fetchRouteWithElevation({
-  coordinates,
+  start,
+  end,
+  distance,
   profile = "cycling-regular",
 }: {
-  coordinates: [number, number][];
+  start: [number, number];
+  end?: [number, number];
+  distance?: number; // Km
   profile?: ORSProfile;
 }) {
   if (!ORS_API_KEY) throw new Error("ORS API key is not configured");
 
-  // Directions endpoint (For route coordinates)
   const directionsUrl = `${ORS_BASE_URL}/directions/${profile}/geojson`;
+
+  // Body if endpoint is defined in form, else get distance from directions API endpoint
+  const body = end
+    ? { coordinates: [start, end] }
+    : {
+        coordinates: [start],
+        options: {
+          round_trip: {
+            length: (distance ?? 10) * 1000, // Km -> Meter
+          },
+        },
+      };
+
+  // Get directions from API endpoint
   const directionsRes = await fetch(directionsUrl, {
     method: "POST",
     headers: {
       Authorization: ORS_API_KEY,
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ coordinates }),
+    body: JSON.stringify(body),
   });
 
   if (!directionsRes.ok) {
@@ -38,12 +55,11 @@ export async function fetchRouteWithElevation({
 
   const routeData = await directionsRes.json();
   const geometry = routeData.features[0]?.geometry;
+  const summary = routeData.features[0]?.properties?.summary;
 
-  if (!geometry) {
-    throw new Error("ORS directions response missing geometry");
-  }
+  if (!geometry) throw new Error("ORS directions response missing geometry");
 
-  // Elevations endpoint (For topography data of direction coordinates)
+  // Get elevations/topography data based on directions
   const elevationRes = await fetch(
     "https://api.openrouteservice.org/elevation/line",
     {
@@ -67,12 +83,21 @@ export async function fetchRouteWithElevation({
 
   const elevationData = await elevationRes.json();
 
+  // Combine directions, elevation and metadata
+  const distanceKm = +(summary?.distance / 1000).toFixed(2);
+  const durationMin = Math.round((summary?.duration ?? 0) / 60);
+
   const routeWithElevation = {
     ...routeData,
     features: [
       {
         ...routeData.features[0],
         geometry: elevationData.geometry,
+        properties: {
+          ...summary,
+          distanceKm,
+          durationMin,
+        },
       },
     ],
   };
