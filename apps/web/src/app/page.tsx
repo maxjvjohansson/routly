033a572cfc71e9e757af30ed55a3a7fd7fb71dbf
maxjvjohansson <max.jv.johansson@gmail.com>
2025-11-10 20:34:54 +0100

    Add: Base Hero component to web-app, also added some mockup images to assets

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import GenerateRouteSection from "src/components/GenerateRouteSection/GenerateRouteSection";
+import Hero from "src/components/Hero/Hero";
 
 export default function HomePage() {
   return (
     <>
+      <Hero />
       <GenerateRouteSection />
     </>
   );
