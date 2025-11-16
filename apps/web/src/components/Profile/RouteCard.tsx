48b7d783a3069b5a7898163fa0fa08126892dc77
maxjvjohansson <max.jv.johansson@gmail.com>
2025-11-16 19:16:05 +0100

    Add: Icons to Profile and Explore route cards (small variants)

--- apps/web/src/components/Profile/RouteCard.tsx
+++ apps/web/src/components/Profile/RouteCard.tsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import { webTheme as theme } from "@routly/ui/theme/web";
 import { Button } from "../Button/Button";
+import RouteInfoItem from "../PreviewRouteCard/RouteInfoItem";
+import { FaClock, FaMountain, FaRoute, FaRegEdit } from "react-icons/fa";
 
 type Props = {
   route: any;
@@ -22,7 +24,6 @@ const Card = styled.div`
 const TitleRow = styled.div`
   display: flex;
   justify-content: space-between;
-  align-items: center;
 `;
 
 const Title = styled.h4`
@@ -34,7 +35,7 @@ const Title = styled.h4`
 const EditButton = styled.button`
   background: transparent;
   border: none;
-  color: ${theme.colors.teal};
+  color: ${theme.colors.black};
   font-size: ${theme.typography.lg};
   cursor: pointer;
   transition: opacity 0.2s;
@@ -67,13 +68,27 @@ export default function RouteCard({
     <Card>
       <TitleRow>
         <Title>{route.name}</Title>
-        <EditButton onClick={() => onRename(route)}>✎</EditButton>
+        <EditButton onClick={() => onRename(route)}>
+          <FaRegEdit size={22} />
+        </EditButton>
       </TitleRow>
 
       <InfoRow>
-        <span>{route.distance_km?.toFixed(1)} km</span>
-        <span>{route.elevation_gain} m</span>
-        <span>{route.duration_estimate?.toFixed(0)} min</span>
+        <RouteInfoItem
+          mode="compact"
+          value={`${route.distance_km?.toFixed(1)} km`}
+          icon={<FaRoute size={18} />}
+        />
+        <RouteInfoItem
+          mode="compact"
+          value={`${route.elevation_gain} m`}
+          icon={<FaMountain size={18} />}
+        />
+        <RouteInfoItem
+          mode="compact"
+          value={`${route.duration_estimate?.toFixed(0)} min`}
+          icon={<FaClock size={18} />}
+        />
       </InfoRow>
 
       <Actions>
